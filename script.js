8813e71ed3850ce5a08ba44c5f7f959e064cdea0
aryar404 <aryarajeev2005@gmail.com>
2025-09-04 18:22:15 +0000

    styles

--- script.js
+++ script.js
@@ -10,9 +10,25 @@ function addTask(){
     return;
    }
   const li=document.createElement("li");
-  li.textContent=taskText;
+  const span=document.createElement("span");
+  span.textContent=taskText;
+  li.appendChild(span);
+
   const list=document.getElementById("todo-list");
   list.appendChild(li);
+  const deleteBtn=document.createElement("button");
+  deleteBtn.textContent="Delete";
+  deleteBtn.style.marginLeft="10px";
+  li.appendChild(deleteBtn);
+  deleteBtn.addEventListener("click",function(){
+    list.removeChild(li)
+  })
+  
+  li.addEventListener("click",function(){
+    console.log("clicked");
+   span.classList.toggle("completed");
+  });
+
   input.value="";
 
 }
